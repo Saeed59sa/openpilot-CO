2d33a3be58c582f580aa0762d1474354714afc45
Saeed59sa <65347537+Saeed59sa@users.noreply.github.com>
2025-10-02 10:29:25 +0400

    Update fingerprints.py

--- opendbc_repo/opendbc/car/hyundai/fingerprints.py
+++ opendbc_repo/opendbc/car/hyundai/fingerprints.py
@@ -967,6 +967,7 @@ FW_VERSIONS = {
     ],
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00DN8 MFC  AT MES LHD 1.00 1.01 99211-L1800 230512',
+    [,
   },
   CAR.HYUNDAI_SONATA_HYBRID: {
     (Ecu.fwdRadar, 0x7d0, None): [
